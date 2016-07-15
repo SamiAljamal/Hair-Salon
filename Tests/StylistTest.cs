8e325af38e9b0371856023baa086ef87c8d5e1d4
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-07-15 08:53:26 -0700

    set up project files.

---
+++ Tests/StylistTest.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Salon
+{
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=salon_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Salon.DeleteAll();
+    }
+  }
+}
